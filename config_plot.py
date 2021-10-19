04836b63ea056781d1f514f95d1d51c6355e2f66
Paolo Baldini <paolobaldini01@gmail.com>
2021-10-19 15:27:25 +0200

    temporary configuration for plotting

---
+++ config_plot.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@author: milan
+edited: Paolo Baldini
+"""
+
+plt_network_adj_matrix = 0
+plot_network_1 = 1     # plot NW network
+plot_network_2 = 1     # plot the NW network graph
+plot_network_3 = 0     # plot the NW network graph kamada_kawai
+plot_network_4 = 0
+plot_network_5 = 0
+plot_network_6 = 1     # plot only the largest connected component
+plot_network_7 = 1
+
+plot_conductance = 1   # plot G-V
+
+voltage_distribution_map = 1
+
+conductance_map = 1
+
+information_centrality_map = 1
+
+animation_1 = 0
