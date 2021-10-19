86b434191369a428805929ca7b57327076faa491
Paolo Baldini <paolobaldini01@gmail.com>
2021-10-19 15:40:04 +0200

    moved network definition to device file

--- NWNetworkSimulator.py
+++ NWNetworkSimulator.py
@@ -5,8 +5,9 @@ import logging
 
 # project files
 from model import NetworkUtils
-from model.NetworkStimulator import NetworkStimulator
+from model.device.default import *
 from model.inspect import print_info, inspect, information_centrality
+from model.NetworkStimulator import NetworkStimulator
 from view import plot
 
 logging.basicConfig(level = logging.DEBUG, format = '[%(asctime)s %(levelname)s]\t %(message)s')
--- simulation.py
+++ simulation.py
@@ -1,35 +1,6 @@
 from NWNetworkSimulator import *
 
 ###############################################################################
-# NW DISTRIBUTION PARAMETERS
-
-nwires = 900#1500
-centroid_dispersion = 200
-
-mean_length = 40.0
-std_length = 14.0
-seed = 40
-
-# todo non constant
-Lx = 500
-Ly = 500
-
-##Source and ground node positions
-sourcenode = 273                                                  
-groundnode = 358
-
-# update_edge_weights parameters
-
-kp0 = 0.0001
-eta_p = 10
-kd0 = 0.5
-eta_d = 1
-
-# admittance
-Y_min = 0.001                                                                  
-Y_max = Y_min*100
-
-###############################################################################
 # NETWORK SETUP
 
 connections = NetworkUtils.generate(
