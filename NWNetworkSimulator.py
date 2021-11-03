bceaeac11078f68707b05fe96b24ccac2ecaa4f6
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-04 00:20:11 +0100

    cleaned main simulator file

--- NWNetworkSimulator.py
+++ NWNetworkSimulator.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
-
-# standard lib
 import logging
-import model.device.devices as devices
+import view.plot as plot
 
-# project files
-from model.device.default import default
-from model.inspect import print_info, inspect, information_centrality
-from model.NetworkStimulator import NetworkStimulator
-
-# from view.view import plot_all
-
+from model.device.Device import Device
+from model.device.datasheet.default import default
+from model.tools.inspect import print_info, inspect
+from model.network.Stimulator import Stimulator
 
 __LOGGING_FORMAT = '[%(asctime)s %(levelname)s]\t %(message)s'
 __all__ = [
-    "logging", "debug_mode",    # logging utils
-    "default", "devices",       # device utils
-    "print_info", "inspect", "information_centrality",
-    "NetworkStimulator"         # simulator
+    "logging", "debug_mode",  # logging utils
+    "default", "Device",  # device data
+    "print_info", "inspect",  # supervision utils
+    "Stimulator",  # network simulator
+    "plot"  # plotting utils
 ]
 
+# define default logging level as INFO and following a standard format
 logging.basicConfig(level=logging.INFO, format=__LOGGING_FORMAT)
 
 
+# enable the debug mode in the simulator (i.e., log at DEBUG level)
 def debug_mode():
     logging.basicConfig(level=logging.DEBUG, format=__LOGGING_FORMAT)
