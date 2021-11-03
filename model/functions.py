8b00d0fbe785996c698d8e46afbeb29f2f887e7a
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-03 21:05:51 +0100

    cleaned function file from (now) useless code

--- model/functions.py
+++ model/functions.py
@@ -5,238 +5,111 @@ Created on Wed Nov 27 08:06:18 2019
 
 @author: Gianluca
 """
-
-import os
 import math
 import random
 from networkx import grid_graph
-import collections
-import numpy as np
-import matplotlib.pyplot as plt
 
 import networkx as nx
 
 
-####################    -    GRAPH DEFINITION     -    ########################
-def define_grid_graph(xdim, ydim):
+# GRAPH DEFINITION
+def __define_grid_graph(xdim, ydim, config=lambda _: _):
+    graph = config(grid_graph(dim=[xdim, ydim]))
 
-    Ggrid = grid_graph(dim = [xdim, ydim])
-    G = nx.convert_node_labels_to_integers(Ggrid, first_label = 0, ordering = 'default', label_attribute = 'pos')
-
-    return G
-
-###########  -    GRAPH DEFINITION_2  (with random diagonals) -    ############
-
-
-def define_grid_graph_2(xdim,ydim,groundnode_pos,sourcenode_pos):
-
-    ##define a grid graph
-
-    Ggrid = grid_graph(dim = [xdim, ydim])
-    
-    ##define random diagonals
-    for x in range (xdim-1):
-        for y in range(ydim-1):
-            k = random.randint(0, 1)
-            if k == 0:
-                Ggrid.add_edge((x, y), (x+1, y+1))
-            else:
-                Ggrid.add_edge((x+1, y), (x, y+1))
-            
-
-    ##define a graph with integer nodes and positions of a grid graph
-    G = nx.convert_node_labels_to_integers(Ggrid, first_label = 0, ordering = 'default', label_attribute = 'pos')
+    return nx.convert_node_labels_to_integers(
+        graph,
+        first_label=0,
+        ordering='default',
+        label_attribute='pos'
+    )
 
 
+define_grid_graph = __define_grid_graph
 
 
+# GRAPH DEFINITION_2  (with random diagonals)
+def define_grid_graph_2(xdim, ydim):
+    def init(graph):
+        for x in range(xdim - 1):
+            for y in range(ydim - 1):
+                k = random.randint(0, 1)
+                if k == 0:
+                    graph.add_edge((x, y), (x + 1, y + 1))
+                else:
+                    graph.add_edge((x + 1, y), (x, y + 1))
 
-########################  - GRAPH INITIALIZATION     -    #####################
-    
-def initialize_graph_attributes(G,Yin):
-    #add the initial conductance
-    for u,v in G.edges():
-        G[u][v]['Y'] = Yin            #assign initial admittance to all edges                                                         #assign initial high resistance state in all junctions
-        G[u][v]['R'] = 1/G[u][v]['Y']
+    return __define_grid_graph(xdim, ydim, init)
+
+
+# GRAPH INITIALIZATION
+def initialize_graph_attributes(G, Yin):
+    # add the initial conductance
+    for u, v in G.edges():
+        # assign initial admittance to all edges
+        G[u][v]['Y'] = Yin
+
+        # assign initial high resistance state in all junctions
+        G[u][v]['R'] = 1 / G[u][v]['Y']
+
         G[u][v]['deltaV'] = 0
         G[u][v]['g'] = 0
-        
-        
-    ##initialize
+
+    # initialize
     for n in G.nodes():
         G.nodes[n]['pad'] = False
         G.nodes[n]['source_node'] = False
         G.nodes[n]['ground_node'] = False
-        
+
     return G
 
+
 ###############################################################################
 
+# CALCULATE NETWORK RESISTANCE
+def calculate_network_resistance(H, source_node):
+    return H.nodes[source_node]['V'] / calculate_source_current(H, source_node)
 
 
+# CALCULATE V SOURCE
+def calculate_source_voltage(H, source_node):
+    return H.nodes[source_node]['V']
 
 
+# CALCULATE I SOURCE
+def calculate_source_current(H, source_node):
+    source_current = 0
+    for u, v in H.edges(source_node):
+        source_current += H[u][v]['I']
 
-#################  - MODIFIED VOLTAGE NODE ANALYSIS     -    ##################
+    return source_current
 
-def mod_voltage_node_analysis(G, Vin, sourcenode, groundnode):
-    ## MODIFIED VOlTAGE NODE ANALYSIS
-
-    # definition of matrices
-    matZ = np.zeros(shape = (G.number_of_nodes(), 1))
-    matG = np.zeros(shape = (G.number_of_nodes()-1, G.number_of_nodes()-1))
-    matB = np.zeros(shape = (G.number_of_nodes()-1, 1))
-    matD = np.zeros(shape = (1, 1))
-
-    # filling Z matrix
-    matZ[-1] = Vin
-
-    # filling Y matrix as a combination of G B D in the form [(G B) ; (B' D)]
-
-    # elements of G
-    for k in range(0, G.number_of_nodes()):
-        if k < groundnode:
-            nodeneighbors = list(G.neighbors(k))  # list of neighbors nodes
-            for m in range(0, len(nodeneighbors)):
-                matG[k][k] = matG[k][k] + G[k][nodeneighbors[m]]['Y']  # divided by 1
-                if nodeneighbors[m] is not groundnode and nodeneighbors[m]<groundnode:
-                    matG[k][nodeneighbors[m]] = -G[k][nodeneighbors[m]]['Y']
-                if nodeneighbors[m] is not groundnode and nodeneighbors[m]>groundnode:
-                    matG[k][nodeneighbors[m]-1] = -G[k][nodeneighbors[m]]['Y']
-        if k > groundnode:
-            nodeneighbors = list(G.neighbors(k))  # list of neighbors nodes
-            for m in range(0, len(nodeneighbors)):
-                matG[k-1][k-1] = matG[k-1][k-1] + G[k][nodeneighbors[m]]['Y']  # divided by 1
-                if nodeneighbors[m] is not groundnode and nodeneighbors[m]<groundnode:
-                    matG[k - 1][nodeneighbors[m]] = -G[k][nodeneighbors[m]]['Y']
-                if nodeneighbors[m] is not groundnode and nodeneighbors[m]>groundnode:
-                    matG[k - 1][nodeneighbors[m]-1] = -G[k][nodeneighbors[m]]['Y']
-    # matB
-    if sourcenode < groundnode:
-        matB[sourcenode] = 1
-    elif sourcenode > groundnode:
-        matB[sourcenode-1] = 1
-
-    # matY
-    submat1 = np.hstack((matG, matB))
-    submat2 = np.hstack((np.transpose(matB), matD))
-    matY = np.vstack((submat1, submat2))
-
-    # solve X matrix from Yx = z
-    invmatY = np.linalg.inv(matY)  # inverse of matY
-    
-    matX = np.matmul(invmatY, matZ)  # Ohm law
-
-    # add voltage as a node attribute
-    for n in G.nodes():
-        if n == groundnode:
-            G.nodes[n]['V'] = 0
-        elif n < groundnode:
-            G.nodes[n]['V'] = matX[n][0]
-        elif n > groundnode:
-            G.nodes[n]['V'] = matX[n - 1][0]
-            
-    ###DEFINE CURRENT DIRECTION
-
-    # transform G to a direct graph H
-
-    H = G.to_directed()  # transform G to a direct graph
-
-    # add current as a node attribute
-
-    for u, v in H.edges():
-        H[u][v]['I'] = (H.nodes[u]['V'] - H.nodes[v]['V']) * H[u][v]['Y']
-        H[u][v]['Irounded'] = np.round(H[u][v]['I'], 2)
-
-    # set current direction
-    for u in H.nodes():  # select current direction
-        for v in H.nodes():
-            if H.has_edge(u, v) and H.has_edge(v, u):
-                if H[u][v]['I'] < 0:
-                    H.remove_edge(u, v)
-                else:
-                    H.remove_edge(v, u)
-
-    return H
 
 ###############################################################################
 
 
+# UPDATE EDGE WEIGHT (Miranda's model)
+def update_edge_weights(device, datasheet, delta_t):
+    graph = device.connected_nodes[0]
+    for u, v in graph.edges():
+        edge = graph[u][v]
 
-#################  - CALCULATE  NETWORK RESISTANCE     -    ####################
-    
-
-def calculate_network_resistance(H, sourcenode):
-    
-    I_fromsource = 0
-    for u,v in H.edges(sourcenode):
-        a = H[u][v]['I']
-        I_fromsource = I_fromsource+a
-
-    
-    Rnetwork = H.nodes[sourcenode]['V']/I_fromsource
-    
-    return Rnetwork
-
-###############################################################################
-
-
-#######################  - CALCULATE V source     -    ########################
-    
-    
-def calculate_Vsource(H, sourcenode):
-
-    Vsource = H.nodes[sourcenode]['V']
-    
-    return Vsource
-
-###############################################################################
+        edge['deltaV'] = abs(graph.nodes[u]['V'] - graph.nodes[v]['V'])
+        edge['kp'] = datasheet.kp0 * math.exp(datasheet.eta_p * edge['deltaV'])
+        edge['kd'] = datasheet.kd0 * math.exp(-datasheet.eta_d * edge['deltaV'])
+        edge['g'] = (
+                edge['kp'] / (edge['kp'] + edge['kd'])
+            ) * (
+                1 - (
+                    1 - (1 + (edge['kd'] / edge['kp']) * edge['g'])
+                ) * math.exp(
+                    -(edge['kp'] + edge['kd']) * delta_t
+                )
+            )
+        edge['Y'] = datasheet.Y_min * (1 - edge['g']) + datasheet.Y_max * edge['g']
+        edge['R'] = 1 / edge['Y']
 
 
-
-
-
-#######################  - CALCULATE I source     -    ########################
-
-def calculate_Isource(H, sourcenode):
-    
-    I_from_source = 0
-    for u,v in H.edges(sourcenode):
-        a = H[u][v]['I']
-        I_from_source = I_from_source+a
-    
-    return I_from_source
-
-###############################################################################
-    
-
-
-
-
-#################  - UPDATE EDGE WEIGHT (Miranda's model)   -    ##############
-    
-def update_edge_weigths(G,delta_t,Y_min, Y_max,kp0,eta_p,kd0,eta_d):
-    
-
-    for u,v in G.edges():
-        
-        G[u][v]['deltaV'] = abs(G.nodes[u]['V']-G.nodes[v]['V'])
-    
-        G[u][v]['kp'] = kp0*math.exp(eta_p*G[u][v]['deltaV'])
-        
-        G[u][v]['kd'] = kd0*math.exp(-eta_d*G[u][v]['deltaV'])
-        
-        G[u][v]['g'] = (G[u][v]['kp']/(G[u][v]['kp']+G[u][v]['kd']))*(1-(1-(1+(G[u][v]['kd']/G[u][v]['kp'])*G[u][v]['g']))*math.exp(-(G[u][v]['kp']+G[u][v]['kd'])*delta_t))
-    
-        G[u][v]['Y'] = Y_min*(1-G[u][v]['g'])+Y_max*G[u][v]['g']
-        
-        G[u][v]['R'] = 1/G[u][v]['Y']
-    
-    return G
-
-######################  - Find connected components   -    ####################
-
+# FIND CONNECTED COMPONENTS
 def connected_component_subgraphs(G):
     for c in nx.connected_components(G):
         yield G.subgraph(c)
-
