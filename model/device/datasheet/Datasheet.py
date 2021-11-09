79eb0cc303f17915f2a7953e4889225a31d2e539
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-09 22:19:26 +0100

    changed datasheet files

--- model/device/datasheet/Datasheet.py
+++ model/device/datasheet/Datasheet.py
@@ -5,22 +5,25 @@ from dataclasses import dataclass
 class Datasheet:
     """Define the static properties of the device"""
 
-    wires_count: int
-    centroid_dispersion: int
-    mean_length: float
-    std_length: float
-    seed: int
+    wires_count: int = 1500
+    centroid_dispersion: int = 200
+    mean_length: float = 40.0
+    std_length: float = 14.0
+    seed: int = 40
 
     # device size
-    Lx: int
-    Ly: int
+    Lx: int = 500
+    Ly: int = 500
 
     # update_edge_weights parameters
-    kp0: float
-    eta_p: int
-    kd0: float
-    eta_d: int
+    kp0: float = 0.0001
+    eta_p: int = 10
+    kd0: float = 0.5
+    eta_d: int = 1
 
     # admittance
-    Y_min: float
-    Y_max: float
+    Y_min: float = 0.001
+    Y_max: float = 0.001 * 100
+
+
+default = Datasheet
---
+++ model/device/datasheet/constructor.py
@@ -0,0 +1,11 @@
+from model.device.datasheet.Datasheet import Datasheet
+
+
+def from_dict(dictionary: dict) -> Datasheet:
+    """Return a Datasheet from its dict representation"""
+
+    datasheet = Datasheet()
+
+    datasheet.__dict__.update(dictionary)
+
+    return datasheet
