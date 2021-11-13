79ad23d409e3c594fe35448186687e37b69c83e8
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-13 17:33:01 +0100

    added support to loads in evolution class

--- model/analysis/evolution.py
+++ model/analysis/evolution.py
@@ -1,45 +1,63 @@
 import networkx as nx
 
+from dataclasses import dataclass, field
 from functools import cache
 from collections.abc import Iterable
 from networkx import Graph
-
+from typing import Set, Tuple, List
 from model.analysis.utils import calculate_currents
 from model.device.datasheet.Datasheet import Datasheet
 
 
+@dataclass
 class Evolution:
     """Contains the evolution/history of the network"""
 
-    network_instances: [Graph] = []  # contains the evolution in time of the net
+    # datasheet of the stimulated device
+    datasheet: Datasheet
 
-    def __init__(
-            self,
-            datasheet: Datasheet,
-            wires_dict: dict,
-            ground: int,
-            delta_time: float
-    ):
-        self.datasheet = datasheet
-        self.wires_dict = wires_dict
-        self.ground = ground
-        self.delta_t = delta_time
+    # wires dictionary of the device
+    wires_dict: dict
+
+    # network stimulation delay
+    delta_time: float
+
+    # list of nodes directly connected to ground.
+    # may be empty if the grounds are serial to the loads:
+    #   Source --- [chip] --- Load --- Ground
+    grounds: Set[int] = field(default_factory=set)
+
+    # list of nodes on which there are external loads attached.
+    # may be empty if the simulation does not include loads
+    # the integer is the node identifier, the float the load resistance
+    loads: Set[Tuple[int, float]] = field(default_factory=set)
+
+    # contains the evolution in time of the network
+    network_instances: List[Tuple[Graph, List[Tuple[int, float]]]] =\
+        field(default_factory=list)
 
     def append(self, graph: Graph, stimulus: [(int, float)]):
         """Add a graph (i.e., network state) to the history"""
 
         self.network_instances.append((graph.copy(), stimulus))
 
-    @cache
     def currents_graphs(self, reverse=False) -> Iterable[Graph]:
         """Get currents flow in the graphs. Apply in a lazy way"""
 
-        graphs = [g for g, _ in self.network_instances]
+        @cache
+        def _():
+            """
+            Needed for caching: the class is not hashable and then it cannot
+            used as an input parameter/key of the input-result save
+            """
+            graphs = [g for g, _ in self.network_instances]
 
-        if reverse:
-            graphs = reversed(graphs)
+            if reverse:
+                graphs = reversed(graphs)
 
-        return map(calculate_currents, graphs)
+            return map(calculate_currents, graphs)
+
+        return _()
 
     def information_centrality(self):
         """Return information centrality measure for the network evolution"""
