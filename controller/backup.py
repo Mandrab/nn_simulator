9fcae5f188c7fe3942efd7860be7a63f7e44e568
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-15 10:23:12 +0100

    corrected bug in saving file

--- controller/backup.py
+++ controller/backup.py
@@ -3,6 +3,7 @@ import json
 import logging
 
 import networkx as nx
+import numpy as np
 
 from model.device.datasheet.Datasheet import Datasheet
 from model.device.datasheet.factory import from_dict
@@ -26,36 +27,24 @@ def save(
 
     logging.info("Saving graph to file")
 
-    # convert wires dict to correct format
-    wires = dict(
-        xa=wires['xa'].tolist(),
-        ya=wires['ya'].tolist(),
-        xc=wires['xc'].tolist(),
-        yc=wires['yc'].tolist(),
-        xb=wires['xb'].tolist(),
-        yb=wires['xa'].tolist(),
-        theta=wires['theta'].tolist(),
-        avg_length=wires['avg_length'],
-        wire_lengths=wires['wire_lengths'].tolist(),
-        dispersion=wires['dispersion'],
-        centroid_dispersion=wires['centroid_dispersion'],
-        gennorm_shape=wires['gennorm_shape'],
-        this_seed=wires['this_seed'],
-        outside=wires['outside'].tolist(),
-        length_x=wires['length_x'],
-        length_y=wires['length_y'],
-        number_of_wires=wires['number_of_wires'],
-        wire_distances=wires['wire_distances'].tolist()
-    )
+    # remove a saved instance of the graph from the wires-dict
+    del wires['G']
 
-    data = [
+    # convert wires dict to correct format
+    wires = dict([
+        (key, value) if not isinstance(value, np.ndarray)
+        else (key, value.tolist())
+        for key, value in wires.items()
+    ])
+
+    pairs = [
         (datasheet_file, dataclasses.asdict(datasheet)),
         (graph_file, nx.node_link_data(graph)),
         (wires_file, wires)
     ]
 
     # save each data in the file with a json format
-    for file_name, data in data:
+    for file_name, data in pairs:
         with open(file_name, 'w') as file:
             json.dump(data, file)
 
