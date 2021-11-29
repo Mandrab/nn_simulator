6b5fb41f48eb9c6bca775f916709e7c02c25cd14
Paolo Baldini <paolobaldini01@gmail.com>
2021-11-29 09:39:03 +0100

    raise an error if its not possible to get feasible nodes. It makes the error more comprehensible

--- nanowire_network_simulator/model/interface/factory.py
+++ nanowire_network_simulator/model/interface/factory.py
@@ -3,14 +3,19 @@ import random
 from networkx import Graph
 from typing import Set, Tuple, Callable
 
+__NO_VIABLE_NODE_ERROR = "Not enough nodes respect the required properties"
+
 
 def random_nodes(graph: Graph, avoid: Set[int], count: int = 1) -> Set[int]:
     """
     Select random nodes from graph, avoiding the specified ones.
     It can be used, for example, to pick nodes that are not ground.
+    If no viable nodes are present, throw an error.
     """
 
     viable_nodes = [*{*graph.nodes()} - avoid]
+
+    assert len(viable_nodes) > count, __NO_VIABLE_NODE_ERROR
 
     # get a 'count' random available nodes
     return {
