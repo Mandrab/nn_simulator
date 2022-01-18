5c989286555e285ba9af2b7c85cc3553fdeefc0b
Paolo Baldini <paolobaldini01@gmail.com>
2022-01-18 17:16:58 +0100

    moved block of matplotlib logs in logger file

--- main/logger.py
+++ main/logger.py
@@ -6,3 +6,7 @@ __FORMAT = '[%(asctime)s %(levelname)s]\t %(message)s'
 # define default logging level as INFO and following a standard format
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=__FORMAT)
 logger = logging.getLogger('nanowire-network-simulator-lib')
+
+# disable matplotlib logging
+logging.getLogger('matplotlib.font_manager').disabled = True
+logging.getLogger('matplotlib.colorbar').disabled = True
