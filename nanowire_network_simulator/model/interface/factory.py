27134613e3b74c4f7088b8f513b37710b62dedb7
Paolo Baldini <paolobaldini01@gmail.com>
2022-03-30 21:29:15 +0200

    updated random node selection according to changes

--- nanowire_network_simulator/model/interface/factory.py
+++ nanowire_network_simulator/model/interface/factory.py
@@ -1,31 +1,27 @@
 import random
 
-from networkx import Graph
+from nanowire_network_simulator.model.device.network import Network
 from typing import Set, Tuple, Callable
 
 __NO_VIABLE_NODE_ERROR = "Not enough nodes respect the required properties"
 
 
-def random_nodes(graph: Graph, avoid: Set[int], count: int = 1) -> Set[int]:
+def random_nodes(graph: Network, avoid: Set[int], count: int = 1) -> Set[int]:
     """
     Select random nodes from graph, avoiding the specified ones.
     It can be used, for example, to pick nodes that are not ground.
     If no viable nodes are present, throw an error.
     """
 
-    viable_nodes = [*{*graph.nodes()} - avoid]
-
-    assert len(viable_nodes) > count, __NO_VIABLE_NODE_ERROR
+    viable_nodes = len(graph.circuit) - len(avoid)
+    assert viable_nodes > count, __NO_VIABLE_NODE_ERROR
 
     # get a 'count' random available nodes
-    return {
-        viable_nodes.pop(random.randrange(len(viable_nodes)))
-        for _ in range(count)
-    }
+    return set(random.sample(set(range(len(graph.circuit))) - avoid, k=count))
 
 
 def random_loads(
-        graph: Graph,
+        graph: Network,
         avoid: Set[int],
         count: int = 1,
         resistance_supplier: Callable[[], float] = lambda: random.gauss(1, 2)
