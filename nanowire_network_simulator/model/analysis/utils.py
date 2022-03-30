2311fe104f64b70e572d5b609212cab3162e0af2
Paolo Baldini <paolobaldini01@gmail.com>
2022-03-30 18:21:19 +0200

    added network current and voltage analysis utils

--- nanowire_network_simulator/model/analysis/utils.py
+++ nanowire_network_simulator/model/analysis/utils.py
@@ -1,56 +1,37 @@
-import numpy as np
+import cupy as cp
 
-from networkx import Graph
-from typing import List
-
-
-def calculate_currents(graph: Graph) -> Graph:
-    """Define current directions in the network through a directed graph"""
-
-    # transform graph to a direct graph graph
-    graph = graph.to_directed()
-
-    # calculate and add current as a edge attribute
-    for u, v in graph.edges():
-        delta_v = graph.nodes[u]['V'] - graph.nodes[v]['V']
-        edge_admittance = graph[u][v]['Y']
-
-        graph[u][v]['I'] = delta_v * edge_admittance
-        graph[u][v]['i_rounded'] = np.round(graph[u][v]['I'], 2)
-
-    # remove negative arches (there already is a positive, opposite arch)
-    for u in graph.nodes():
-        for v in graph.nodes():
-
-            # if at least one of the arches has been deleted, continue
-            if not graph.has_edge(u, v) or not graph.has_edge(v, u):
-                continue
-
-            # one of the arches has to be removed
-            if graph[u][v]['I'] < 0:
-                graph.remove_edge(u, v)
-            else:
-                graph.remove_edge(v, u)
-
-    return graph
+from nanowire_network_simulator.model.device.network import Network
 
 
-def node_voltage(graph: Graph, node: int) -> float:
-    """Extract voltage value from the specific node"""
+def delta_voltage(network: Network) -> cp.ndarray:
+    """
+    Calculate delta voltage on a junction.
 
-    return graph.nodes[node]['V']
+    Parameters
+    ----------
+    network: NanowireNetwork
+        target of the calculation
+    Returns
+    -------
+    An ndarray representing the voltage difference on each junction
+    """
+
+    partial = network.adjacency * network.voltage
+    partial = network.voltage.reshape(-1, 1) - partial
+    return cp.absolute(network.adjacency * partial)
 
 
-def current_flow(graph: Graph, node: int) -> float:
-    """Calculate current flowing out / exiting a node"""
+def calculate_currents(network: Network) -> cp.ndarray:
+    """
+    Define currents in the network multiplying voltages and conductances.
 
-    return sum(filter(
-        lambda _: _ > 0,    # take only positive currents (outgoing)
-        [graph[u][v]['I'] for u, v in graph.edges(node)]
-    ))
+    Parameters
+    ----------
+    network: Network
+        the nanowire network in which calculate the currents
+    Returns
+    -------
+    A matrix with the current value specified in the node-node intersection
+    """
 
-
-def calculate_network_currents(graph: Graph, sources: List[int]) -> float:
-    """Calculate the currents entering the network (from sources)"""
-
-    return sum([current_flow(graph, node) for node in sources])
+    return delta_voltage(network) * network.circuit
