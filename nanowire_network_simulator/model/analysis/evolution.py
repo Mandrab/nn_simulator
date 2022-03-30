ffa29515cfbe78ea61e69304073b8aec403a9958
Paolo Baldini <paolobaldini01@gmail.com>
2022-03-30 21:26:45 +0200

    updated evolution according to changes

--- nanowire_network_simulator/model/analysis/evolution.py
+++ nanowire_network_simulator/model/analysis/evolution.py
@@ -1,103 +1,106 @@
+import cupy as cp
 import networkx as nx
 
+from ..device.networks import nn2nx
+from .utils import calculate_currents
+from nanowire_network_simulator.model.device.network import Network as Nw, copy
 from dataclasses import dataclass, field
 from functools import cached_property
-from collections.abc import Iterable
+from collections.abc import Iterable, Generator
 from nanowire_network_simulator.model.device import Datasheet
-from networkx import Graph
 from typing import Set, Tuple, List, Dict
-from .utils import calculate_currents
 
 
 @dataclass
 class Evolution:
-    """Contains the evolution/history of the network"""
+    """
+    Contains the evolution/history of the network
 
-    # datasheet of the stimulated device
+    Parameters
+    ----------
     datasheet: Datasheet
-
-    # wires dictionary of the device
+        datasheet of the stimulated device
     wires_dict: Dict
-
-    # network stimulation delay
+        wires dictionary of the device
     delta_time: float
+        network stimulation delay
+    grounds: Set[int]
+        list of nodes directly connected to ground.
+        may be empty if the grounds are serial to the loads:
+        Source --- [chip] --- Load --- Ground
+    loads: Set[Tuple[int, float]]
+        list of nodes on which there are external loads attached.
+        may be empty if the simulation does not include loads
+        the integer is the node identifier, the float the load resistance
+    network_instances: List[Tuple[NN, List[Tuple[int, float]]]]
+        contains the evolution in time of the network
+    """
 
-    # list of nodes directly connected to ground.
-    # may be empty if the grounds are serial to the loads:
-    #   Source --- [chip] --- Load --- Ground
+    datasheet: Datasheet
+    wires_dict: Dict
+    delta_time: float
     grounds: Set[int] = field(default_factory=set)
-
-    # list of nodes on which there are external loads attached.
-    # may be empty if the simulation does not include loads
-    # the integer is the node identifier, the float the load resistance
     loads: Set[Tuple[int, float]] = field(default_factory=set)
+    instances: List[Tuple[Nw, Dict[int, float]]] = field(default_factory=list)
 
-    # contains the evolution in time of the network
-    network_instances: List[Tuple[Graph, List[Tuple[int, float]]]] =\
-        field(default_factory=list)
-
-    def append(self, graph: Graph, stimulus: [(int, float)]):
+    def append(self, graph: Nw, stimulus: [(int, float)]):
         """
         Add a graph (i.e., network state) to the history.
         The graph is copied before being add.
         """
 
-        self.network_instances.append((graph.copy(), stimulus))
+        self.instances.append((copy(graph), dict(stimulus)))
 
-    def currents_graphs(self, reverse: bool = False) -> Iterable[Graph]:
+    def currents_graphs(self, reverse: bool = False) -> Generator:
         """Get currents flow in the graphs. Apply in a lazy way"""
 
-        graphs = [g for g, _ in self.network_instances]
+        graphs = reversed(self.instances) if reverse else self.instances
 
-        if reverse:
-            graphs = reversed(graphs)
+        def result():
+            for n, _ in graphs:
+                n.currents = calculate_currents(n)
+                yield n
+        return result()
 
-        return map(calculate_currents, graphs)
-
-    def information_centrality(self, reverse: bool = False) -> Iterable[Graph]:
+    def information_centrality(
+            self, reverse: bool = False
+    ) -> Iterable[cp.ndarray]:
         """Return information centrality measure for the network evolution"""
 
-        graphs = [g for g, _ in self.network_instances]
+        graphs = [g for g, _ in self.instances]
 
         if reverse:
             graphs = reversed(graphs)
 
-        def _(graph: Graph):
-            currents_graph = calculate_currents(graph)
-
-            nx.set_node_attributes(
-                currents_graph,
-                nx.information_centrality(graph, weight='Y'),
-                'information_centrality'
-            )
-
-            return currents_graph
+        def _(network: Nw) -> cp.ndarray:
+            centrality = nx.information_centrality(nn2nx(network), weight='Y')
+            return cp.asarray(list(centrality.values()))
 
         return map(_, graphs)
 
     @property
-    def graph(self) -> Graph:
+    def graph(self) -> Nw:
         """Returns the most updated graph in the history"""
 
-        return self.network_instances[-1][0]
+        return self.instances[-1][0]
 
     @property
-    def inputs(self) -> List[Tuple[int, float]]:
+    def inputs(self) -> Dict[int, float]:
         """Return the latest stimulation values"""
 
-        return self.network_instances[-1][1]
+        return self.instances[-1][1]
 
     @property
     def sources(self) -> Set[int]:
         """Return the latest stimulation values"""
 
-        return {s for s, _ in self.network_instances[-1][1]}
+        return {s for s, _ in self.inputs.items()}
 
     @cached_property
     def duration(self):
         """Return the time elapsed between the first and last update"""
 
-        return range(len(self.network_instances))
+        return range(len(self.instances))
 
     @cached_property
     def update_times(self):
