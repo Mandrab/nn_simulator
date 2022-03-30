00abe8264d40bb26c9bee5c523a280d0ac7d8154
Paolo Baldini <paolobaldini01@gmail.com>
2022-03-30 17:40:50 +0200

    moved and simplified network creation logic

--- nanowire_network_simulator/model/device/networks.py
+++ nanowire_network_simulator/model/device/networks.py
@@ -1,6 +1,46 @@
 import networkx as nx
 
+from .datasheet.Datasheet import Datasheet
+from .wires import generate_wires_distribution, detect_junctions, generate_graph
 from nanowire_network_simulator.model.device.network import Network
+from nanowire_network_simulator.logger import logger
+from typing import Dict
+
+
+def generate_network_data(datasheet: Datasheet) -> Dict:
+    """
+    Generate the data of the network according to the datasheet specifications.
+
+    Parameters
+    ----------
+    datasheet: Datasheet
+        the technical description of the nanowire network
+    Returns
+    -------
+    A dictionary with the physical information about the nanowire network
+    """
+
+    logger.info('Generating network data')
+
+    # generate the network
+    wires_dict = generate_wires_distribution(
+        number_of_wires=datasheet.wires_count,
+        wire_av_length=datasheet.mean_length,
+        wire_dispersion=datasheet.std_length,
+        general_normal_shape=10,
+        centroid_dispersion=datasheet.centroid_dispersion,
+        seed=datasheet.seed,
+        Lx=datasheet.Lx,
+        Ly=datasheet.Ly
+    )
+
+    # get junctions list and their positions
+    detect_junctions(wires_dict)
+
+    # generate graph object and adjacency matrix
+    generate_graph(wires_dict)
+
+    return wires_dict
 
 
 def nn2nx(network: Network) -> nx.Graph:
