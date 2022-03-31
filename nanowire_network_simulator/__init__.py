7960f4a84de97e53d0e876fb5b4b726948b745f0
Paolo Baldini <paolobaldini01@gmail.com>
2022-03-31 11:59:51 +0200

    added and removed imports from main __init__

--- nanowire_network_simulator/__init__.py
+++ nanowire_network_simulator/__init__.py
@@ -3,13 +3,12 @@ from .controller import backup
 from .model.analysis.evolution import Evolution
 from .model.analysis.measures import print_info, inspect
 from .model.device.datasheet.Datasheet import default
-from .model.device.factory import generate_network, get_graph, generate_graph
-from .model.device.factory import minimum_viable_network
-from .model.device.utils import initialize_graph_attributes, largest_component
+from .model.device.network import connect, nanowire_network
+from .model.device.networks import generate_network_data
 from .model.interface.factory import random_nodes, random_loads
 from .model.interface.evolutor import mutate, non_ground_selection
 from .model.interface.evolutor import minimum_distance_selection
-from .model.stimulator import stimulate, voltage_initialization
+from .model.stimulator import stimulate
 from .view import plot
 
 LOGGER_NAME = 'nanowire-network-simulator-lib'
@@ -22,15 +21,13 @@ __all__ = [
     "print_info", "inspect",    # supervision utils
     # device utilities & configurations
     "default",
-    # network creation
-    "generate_network", "get_graph", "generate_graph", "minimum_viable_network",
-    # network initialization
-    "initialize_graph_attributes", "largest_component",
+    # nanowire networks operation/utils
+    "connect", "nanowire_network", "generate_network_data",
     # interface / connection definition
     "random_nodes", "random_loads", "mutate", "non_ground_selection",
     "minimum_distance_selection",
     # stimulation utilities for the network
-    "stimulate", "voltage_initialization",
+    "stimulate",
     # logging utilities & setups
     "LOGGER_NAME",
     # plotting utils
