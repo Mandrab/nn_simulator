86dbec9809d6b7b57f3057dd6fceafcfc1595499
Paolo Baldini <paolobaldini01@gmail.com>
2022-04-03 15:02:22 +0200

    added tests in stimulator

--- test/model/stimulator.py
+++ test/model/stimulator.py
@@ -1,61 +1,46 @@
 import cupy as cp
 
+from nanowire_network_simulator import default
 from nanowire_network_simulator.model.device.network import Network
 from nanowire_network_simulator.model.stimulator import \
-    modified_voltage_node_analysis
-
+    modified_voltage_node_analysis, update_conductance
 
 samples = [
     (
         # [V]|---/\/[R]\/\---[A]---/\/[R]\/\---|[G]
-        #
-        # Expected voltage distribution:
-        # - V: 5.0
-        # - A: 2.5
-        # - G: 0.0
-        cp.array([
+        [
             [0, 1, 0],
             [1, 0, 1],
             [0, 1, 0]
-        ]),
+        ],
+        # expected voltage distribution
         [5.00, 2.50]
     ),
     (
         #      |--/\/[R]\/\---[A]---/\/[R]\/\--|
         # [V]--|                               |--[G]
         #      |--/\/[R]\/\---[B]---/\/[R]\/\--|
-        #
-        # Expected voltage distribution:
-        # - V: 5.0
-        # - A: 2.5
-        # - B: 2.5
-        # - G: 0.0
-        cp.array([
+        [
             [0, 1, 1, 0],
             [1, 0, 0, 1],
             [1, 0, 0, 1],
             [0, 1, 1, 0]
-        ]),
+        ],
+        # expected voltage distribution
         [5.00, 2.50, 2.50]
     ),
     (
         #                      |--/\/[2R]\/\--[B]--/\/[R]\/\--|
         # [V]---/\/[1.5R]\/\--[A]                             |--[G]
         #                      |--/\/[R]\/\--[C]--/\/[2R]\/\--|
-        #
-        # Expected voltage distribution:
-        # - V: 10.0
-        # - A: 5.0
-        # - B: 1.67
-        # - C: 3.33
-        # - G: 0.0
-        cp.array([
+        [
             [0,          1 / 1.5,    0,        0,        0],
             [1 / 1.5,    0,          1 / 2,    1,        0],
             [0,          1 / 2,      0,        0,        1],
             [0,          1,          0,        0,        1 / 2],
             [0,          0,          1,        1 / 2,    0]
-        ]),
+        ],
+        # expected voltage distribution
         [5.00, 2.50, 0.83333, 1.66666]
     )
 ]
@@ -63,12 +48,12 @@ samples = [
 
 def evaluate_mna_samples(circuit: cp.ndarray, expected_result: cp.ndarray):
     network = Network(
-        adjacency=cp.zeros((1, 1)),
+        adjacency=cp.zeros_like(circuit),
         wires_position=tuple(), junctions_position=tuple(),
         circuit=circuit,
-        admittance=cp.zeros((1, 1)),
-        voltage=cp.zeros((1, 1)),
-        ground_count=1
+        admittance=cp.zeros_like(circuit),
+        voltage=cp.zeros_like(circuit),
+        grounds=1
     )
     modified_voltage_node_analysis(network, {0: 5.00})
     assert cp.allclose(network.voltage[:-1], expected_result)
@@ -76,7 +61,61 @@ def evaluate_mna_samples(circuit: cp.ndarray, expected_result: cp.ndarray):
 
 def test_samples():
     for circuit, expected_result in samples:
-        evaluate_mna_samples(circuit, cp.asarray(expected_result))
+        evaluate_mna_samples(cp.asarray(circuit), cp.asarray(expected_result))
+
+
+def test_non_stimulated_change():
+    circuit = cp.array([
+        [0, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0]
+    ], dtype=cp.float32)
+    network = Network(
+        adjacency=circuit.copy(),
+        wires_position=tuple(), junctions_position=tuple(),
+        circuit=circuit,
+        admittance=cp.zeros((1, 1)),
+        voltage=cp.zeros((1, 1)),
+        grounds=1
+    )
+
+    # update the network to let the simulator converge to stable values
+    update_conductance(network, default, 1.0)
+    initial = network.circuit.copy()
+
+    # update an additional time and get the network state
+    update_conductance(network, default, 1.0)
+    assert cp.allclose(initial, network.circuit, atol=10e-3)
+
+
+def test_stimulated_change():
+    circuit = cp.array([
+        [0, 1, 0, 0],
+        [1, 0, 1, 0],
+        [0, 1, 0, 1],
+        [0, 0, 1, 0]
+    ], dtype=cp.float32)
+    network = Network(
+        adjacency=circuit.copy(),
+        wires_position=tuple(), junctions_position=tuple(),
+        circuit=circuit,
+        admittance=cp.zeros((1, 1)),
+        voltage=cp.zeros((1, 1)),
+        grounds=1
+    )
+
+    # update the network to let the simulator converge to stable values
+    update_conductance(network, default, 1.0)
+    initial = network.circuit.copy()
+
+    # stimulate the network with a non-zero input signal
+    modified_voltage_node_analysis(network, {0: 5.00})
+
+    # update an additional time and get the network state
+    update_conductance(network, default, 0.1)
+    assert not cp.allclose(initial, network.circuit, atol=10e-3)
 
 
 test_samples()
+test_non_stimulated_change()
+test_stimulated_change()
