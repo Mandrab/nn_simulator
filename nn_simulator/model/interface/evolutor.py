eeb0b780d990a35e0706e98b09698c5d779df91d
Paolo Baldini <paolobaldini01@gmail.com>
2022-04-05 09:51:55 +0200

    adapted function to changes and added documentation

--- nn_simulator/model/interface/evolutor.py
+++ nn_simulator/model/interface/evolutor.py
@@ -1,84 +1,125 @@
+import cupy as cp
 import random
 
-from functools import reduce
-from networkx import Graph
+from nn_simulator.model.device.network import Network
 from typing import Callable, List, Set
 
 
-def non_ground_selection(graph: Graph, _: List[int], ground: int) -> Set[int]:
-    """Returns non-ground nodes for the mutation"""
+def non_ground_selection(network: Network) -> Set[int]:
+    """
+    Returns non-ground nodes for the mutation.
 
-    return {*graph.nodes} - {ground}
+    Parameters
+    ----------
+    network: Network
+        The network from which take the nodes
+    Returns
+    -------
+    A set of non-ground nodes.
+    """
+
+    return set(range(network.wires))
 
 
 def minimum_distance_selection(
-        outputs: Set[int],
-        distance: int,
+        outputs: List[int],
+        distance: int = 0,
         negate: bool = False
-) -> Callable[[Graph, List[int], int], Set[int]]:
+) -> Callable[[Network, List[int]], Set[int]]:
     """
     Returns a function for the viable nodes selection for the mutation.
-    Non-ground nodes and nodes that are 'distance' step near to an output node
+    Ground nodes and nodes that are 'distance'-step near to an output node
     are not selected.
-    If 'negate' is True, the function is `negated` and the returned nodes
-    are the grounds, outputs and neighbor ones.
+
+    Parameters
+    ----------
+    outputs: List[int]
+        Indexes of output nodes (the ones that we have to be far from)
+    distance: int
+        Minimum distance from an output to be considered a viable node
+    negate: bool
+        If True, the function is `negated` and the returned nodes are the
+        grounds, outputs and neighbor. If it is False, the returned node are the
+        others.
+    Returns
+    -------
+    A function that calculates the viable nodes to be selected in a network. It
+    takes the Network as input, as well as another value. That is not needed in
+    this logic, and only exists to personalise more similar functions,
+    maintaining a template of the parameter types.
     """
 
-    def _(graph: Graph, _: List[int], ground: int) -> Set[int]:
+    def _(network: Network, _: List[int]) -> Set[int]:
 
-        # remove ground node from the viable ones
-        viable_nodes = {*graph.nodes} - {ground}
+        # create a mask of viable nodes (false = viable; initially all)
+        viable = cp.zeros(network.nodes - network.external_grounds, dtype=bool)
+
+        # set unavailable (i.e., true) the specified nodes
+        viable[outputs] = True
 
         # find nodes 'distance' distant from the output
-        def neighbours(node: int, decreased_distance: int) -> Set[int]:
+        def neighbours(mask: cp.ndarray, decreased_distance: int) -> cp.ndarray:
 
             # base condition: distance 0 is empty set
             if decreased_distance <= 0:
-                return set()
+                return mask
 
-            # take distance 1 neighbours
-            adjacent_neighbours = {v for k, v in graph.edges(node)}
+            # set the nodes neighbor as unavailable
+            mask |= cp.sum(network.adjacency[mask], axis=0, dtype=bool)
 
-            # take farther neighbours (neighbours of neighbours)
-            neighbours_neighbours = [
-                neighbours(node, decreased_distance - 1)
-                for node in adjacent_neighbours
-            ]
+            # recur until found all neighbours
+            return neighbours(mask, decreased_distance - 1)
 
-            # reduce to only a set
-            neighbours_neighbours = reduce(
-                lambda f, s: f | s,
-                neighbours_neighbours
-            )
+        # set unavailable the nodes neighbours
+        viable = neighbours(viable, distance)
 
-            return adjacent_neighbours | neighbours_neighbours
+        # negate the boolean values if required
+        if not negate:
+            viable = cp.logical_not(viable)
 
-        # take neighbours nodes (and neighbours of neighbours, etc...)
-        neighbours = [neighbours(output, distance) for output in outputs]
-        neighbours = reduce(lambda f, s: f | s, neighbours)
+        # remove device grounds from list of available nodes
+        if network.device_grounds > 0:
+            viable = viable[:-network.device_grounds]
 
-        if negate:
-            return {*outputs} | neighbours | {ground}
-        return viable_nodes - outputs - neighbours
+        # retrieve nodes indexes
+        return {i for i, v in enumerate(viable) if v}
 
     return _
 
 
 def mutate(
-        graph: Graph,
+        network: Network,
         sources: List[int],
-        ground: int,
         probability: float,
         minimum_mutants: int,
         maximum_mutants: int,
-        viable_node_selection: Callable[[Graph, List[int], int], Set[int]]
+        viable_node_selection: Callable[[Network, List[int]], Set[int]]
 ) -> List[int]:
     """
-    Mutate the input connections of the network in a random way.
-    Each source can change, with a fixed probability, between the non-ground
-    nodes. Multiple sources can insist on the same node.
-    Maximum_mutants must be bigger or equal than minimum_mutants.
-    In case no viable node exists, the returned value will be unchanged.
+    Mutate the input connections of the network in a random way. Each source can
+    change, with a fixed probability, between the non-ground nodes. Multiple
+    sources can insist on the same node. Maximum_mutants must be bigger or equal
+    than minimum_mutants. In case no viable node exists, the returned value will
+    be unchanged.
+
+    Parameters
+    ----------
+    network: Network
+        The network in which mutate the connections
+    sources: List[int]
+        The source node to reconnect
+    probability: float
+        Probability to mute one connection
+    minimum_mutants: int
+        Minimum number of inputs to reconnect, if possible
+    maximum_mutants: int
+        Maximum number of inputs to reconnect, if possible
+    viable_node_selection: Callable[[Network, List[int]], Set[int]]
+        Function to select viable nodes for the replacement/reconnection. It
+        must accept the network and a list of actual sources
+    Returns
+    -------
+    A list with the new nodes connection indexes for each source.
     """
 
     # determine if a node should mutate or not
@@ -114,7 +155,7 @@ def mutate(
         change_mutants_number(maximum_mutants - mutants_count)
 
     # select viable alternative nodes
-    viable_nodes = viable_node_selection(graph, sources, ground)
+    viable_nodes = viable_node_selection(network, sources)
 
     # if source change, take a random node != from itself and the ground
     # if source does not change or if there are not viable nodes, return it
