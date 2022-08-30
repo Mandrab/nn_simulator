769e8e44ca9500224b88843b4027010c6c0d362b
Paolo Baldini <paolobaldini01@gmail.com>
2022-08-31 00:18:18 +0200

    added model files to library entrypoint

---
+++ src/nanowire_network.h
@@ -0,0 +1,12 @@
+/**
+ * @file nanowire_network.h
+ * 
+ * Nanowire network library files.
+ */
+#ifndef __NANOWIRE_NETWORK__
+#define __NANOWIRE_NETWORK__
+
+#include "model/device/datasheet/datasheet.h"
+#include "model/device/datasheet/serialization.h"
+
+#endif
