6a9c35d95573f2bc1d2e890e53f9ca5763da55ee
Paolo Baldini <paolobaldini01@gmail.com>
2022-11-21 14:19:48 +0100

    added device structure

---
+++ src/device/device.jl
@@ -0,0 +1,37 @@
+using CUDA
+
+export Device
+
+Indexes = Pair{Int64, Int64}
+Point = Pair{Float64, Float64}
+Wire = Tuple{
+    Point,  # centroid
+    Point,  # start
+    Point,  # end
+    Float64 # length
+}
+
+"""
+Contains the state of the nanowire network.
+
+# Arguments
+- `wires::Vector{Wire}`: Set of wires and their own information.
+- `junctions::Dict{Indexes, Point}`: Set of wires junctions and their position.
+- `A::CuArray{Bool, 2}`: Adjacency matrix (i.e., junctions) of the device.
+- `G::CuArray{Float64}`: Adjacency matrix with the value of the conductance in each junction.
+- `Y::CuArray{Float64}`: Adjacency matrix with the value of the admittance in each junction.
+- `V::CuArray{Float64}`: Potential (i.e., voltage) of each wire.
+- `grounds::Integer`: Number of nodes to be considered ground (in the rightmost part of the matrix).
+"""
+struct Device
+    wires::Vector{Wire}
+
+    junctions::Dict{Indexes, Point}
+
+    A::CuArray{Bool, 2}
+    G::CuArray{Float64}
+    Y::CuArray{Float64}
+    V::CuArray{Float64}
+
+    grounds::Integer
+end
