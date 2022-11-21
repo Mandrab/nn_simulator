7e36eb2255f28eeb8dfba2776c6ed4e27ddd988c
Paolo Baldini <paolobaldini01@gmail.com>
2022-11-21 17:41:31 +0100

    simplified datasheet description

--- src/device/datasheet/datasheet.jl
+++ src/device/datasheet/datasheet.jl
@@ -9,8 +9,7 @@ Define the static properties of the device.
 - `wires_count::Integer`: Total number of wires in the network.
 - `wires_length_mean::AbstractFloat`: Average wire length in µm.
 - `wires_length_variance::AbstractFloat`: Length of the nano-wire in µm.
-- `Lx::Integer`: Horizontal length of the device in µm.
-- `Ly::Integer`: Vertical length of the device in µm.
+- `size::Integer`: Length of one side of the device package in µm.
 - `kp:AbstractFloat`: One of the junction potentiation coefficient.
 - `eta_p::Integer`: One of the junction potentiation coefficient.
 - `kd::AbstractFloat`: One of the junction depression coefficient.
@@ -25,9 +24,8 @@ Define the static properties of the device.
     wires_length_mean::AbstractFloat = 40.0
     wires_length_variance::AbstractFloat = 14.0
 
-    # device size
-    Lx::Integer = 500
-    Ly::Integer = 500
+    # device package size
+    size::Integer = 500
 
     # update_edge_weights parameters
     kp::AbstractFloat = 0.0001
