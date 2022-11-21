5c0a4090260df01d28e0cd837b68dded6001c28d
Paolo Baldini <paolobaldini01@gmail.com>
2022-11-21 14:17:04 +0100

    added datasheet structure

---
+++ src/device/datasheet/datasheet.jl
@@ -0,0 +1,44 @@
+using Parameters
+
+export Datasheet
+
+"""
+Define the static properties of the device.
+
+# Arguments
+- `wires_count::Integer`: Total number of wires in the network.
+- `wires_length_mean::AbstractFloat`: Average wire length in µm.
+- `wires_length_variance::AbstractFloat`: Length of the nano-wire in µm.
+- `Lx::Integer`: Horizontal length of the device in µm.
+- `Ly::Integer`: Vertical length of the device in µm.
+- `kp:AbstractFloat`: One of the junction potentiation coefficient.
+- `eta_p::Integer`: One of the junction potentiation coefficient.
+- `kd::AbstractFloat`: One of the junction depression coefficient.
+- `eta_d::Integer`: One of the junction depression coefficient.
+- `Y_min::AbstractFloat`: Minimum admittance value.
+- `Y_max::AbstractFloat`: Maximum admittance value.
+- `seed::Integer`: Seed of the random number generator to create reproducible networks.
+"""
+@with_kw struct Datasheet
+    # wires data
+    wires_count::Integer = 1500
+    wires_length_mean::AbstractFloat = 40.0
+    wires_length_variance::AbstractFloat = 14.0
+
+    # device size
+    Lx::Integer = 500
+    Ly::Integer = 500
+
+    # update_edge_weights parameters
+    kp::AbstractFloat = 0.0001
+    eta_p::Integer = 10
+    kd::AbstractFloat = 0.5
+    eta_d::Integer = 1
+
+    # admittance ranges
+    Y_min::AbstractFloat = 0.001
+    Y_max::AbstractFloat = 0.001 * 100
+
+    # random number generator seed
+    seed::Integer = 40
+end
